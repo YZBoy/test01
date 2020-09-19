bc24bd71b872abd44cf273e70ea57339b5983fe2
YZBoy <33754797+YZBoy@users.noreply.github.com>
2020-09-19 11:45:11 +0800

    Add files via upload

---
+++ screen.py
@@ -0,0 +1,15 @@
+"""
+  @Time : 2019/10/28
+  @Authon : Young
+"""
+
+# 手机截屏功能：
+#  1.以当前时间命名，并导出到桌面
+import os
+import random
+import time
+time1 = time.localtime(time.time())
+time2 = time.strftime("%Y%m%d_%H%M%S",time1)
+path = r"/sdcard/pic_"+ time2 +".png"
+os.system(r"adb shell screencap %s" % path)
+os.system(r"adb pull %s" % path)
